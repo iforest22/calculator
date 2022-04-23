8e2382250914c07767f44533a5b50b01fab0b32a
Ian <ian.forest22@gmail.com>
2022-04-22 18:01:43 -0700

    initialize files, add buttons

---
+++ script.js
@@ -0,0 +1,12 @@
+function operate(operator, num1, num2) {
+    if (operator === '+') { return num1 + num2 }
+    if (operator === '-') { return num1 - num2 }
+    if (operator === '*') { return num1 * num2 }
+    if (operator === '/') { return num1 / num2 }
+}
+
+
+
+numberEntered = 
+
+document.querySelector('.display').textContent = `a`
